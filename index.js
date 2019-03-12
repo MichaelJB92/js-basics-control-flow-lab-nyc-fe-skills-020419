79e07db58f2d38efd79e6c0f01acdacee9bd1765
Michael Bey <michael.bey@stu.bmcc.cuny.edu>
2019-03-12 00:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function scuberGreetingForFeet(numberOfFeet){
   if (numberOfFeet <= 400){
     return('This one is on me!')
   } else if(numberOfFeet <= 2000){
-    return()
+    return('I will gladly take your thirty bucks.')
   }
 }
